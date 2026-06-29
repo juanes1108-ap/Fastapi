01e81c2e033cc730cd3b04702b252373654b29b9
Esteban <juanestebananguloperez75@gmail.com>
2026-06-28 19:58:58 -0500

    crud completo de cliente

--- app/enrutadores/clientes.py
+++ app/enrutadores/clientes.py
@@ -22,11 +22,14 @@ async def listar_clientes(sesion: Sesion_dependancia):
 
 @rutas_clientes.get("/clientes/{cliente_id}", response_model=Cliente)
 async def listar_cliente(cliente_id: int, mi_sesion: Sesion_dependancia): 
-    #recorer la lista de clientes y buscar el cliente con el id especificado
-    for i, cliente in enumerate(lista_clientes):
-        if cliente.id == cliente_id:
-            return cliente
-    raise HTTPException(status_code=404, detail="Cliente no encontrado") 
+    cliente_bd = mi_sesion.get(Cliente, cliente_id) 
+    if not cliente_bd:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Cliente no encontrado")
+    return cliente_bd
+        
+
+
+     
 
 #endpoint para crear un cliente y agragar a la lista de clientes
 
@@ -41,21 +44,26 @@ async def crear_cliente(datos_cliente: clientecrear, mi_sesion: Sesion_dependanc
 
 #endpoint para editar un cliente existente y agregar a la lista
 @rutas_clientes.patch("/clientes/{cliente_id}", response_model=Cliente)
-async def editar_cliente(cliente_id: int, datos_cliente: clienteEditar):
-    for i, cliente in enumerate(lista_clientes):
-        if cliente.id == cliente_id:
-            cliente_val = Cliente.model_validate(datos_cliente.model_dump())
-            cliente_val.id = cliente_id
-            lista_clientes[i] = cliente_val
-            return cliente_val
-    raise HTTPException(status_code=404, detail=f"Cliente con {cliente_id} no fue encontrado")
+async def editar_cliente(cliente_id: int, datos_cliente: clienteEditar, mi_sesion:Sesion_dependancia):
+     
+     cliente_bd = mi_sesion.get(Cliente, cliente_id) 
+     if not cliente_bd:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Cliente no encontrado")
+     cliente_dict = datos_cliente.model_dump(exclude_unset = True)
+     cliente_bd.sqlmodel_update(cliente_dict)
+     mi_sesion.add(cliente_bd)
+     mi_sesion.commit()
+     mi_sesion.refresh(cliente_bd)
+     return cliente_bd
 
 
 #endpoint para eliminar un cliente existente
 @rutas_clientes.delete("/clientes/{cliente_id}", response_model=Cliente)
-async def eliminar_cliente(cliente_id: int):
-    for i, cliente in enumerate(lista_clientes):
-        if cliente.id == cliente_id:
-            cliente_eliminado = lista_clientes.pop(i)
-            return cliente_eliminado
-    raise HTTPException(status_code=404, detail=f"Cliente con {cliente_id} no fue encontrado")
+async def eliminar_cliente(cliente_id: int, mi_sesion: Sesion_dependancia):
+    cliente_bd = mi_sesion.get(Cliente, cliente_id) 
+    if not cliente_bd:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Cliente no encontrado")
+    
+    mi_sesion.delete(cliente_bd)
+    mi_sesion.commit()
+    return cliente_bd
